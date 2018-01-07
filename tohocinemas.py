99ecbc9ab0ffa88376719a7d1420e604e33d351a
stash4 <high.mackey@gmail.com>
2018-01-07 22:00:44 +0900

    Add theater_schedule function in tohocinemas.py

--- tohocinemas.py
+++ tohocinemas.py
@@ -5,6 +5,7 @@ Utilities for TOHO Cinemas website
 import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
+import urllib
 
 BASE_URL = 'https://hlo.tohotheater.jp'
 
@@ -41,6 +42,29 @@ def theater_list():
     '''
     unix_time = datetime.now().strftime('%s')
     url = f'{BASE_URL}/responsive/json/theater_list.json?_dc={unix_time}'
+
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    return res.json()
+
+
+def theater_schedule(vg_cd, show_day, term=99, isMember='', enter_kbn=''):
+    '''
+    Get a schedule of a theater specified by vitCD.
+    '''
+    type_ = 'json'
+    unix_time = datetime.now().strftime('%s')
+
+    query = urllib.parse.urlencode({
+        '__type__': type_,
+        'vg_cd': str(vg_cd),
+        'show_day': str(show_day),
+        'term': str(term),
+        'isMember': isMember,
+        'enter_kbn': enter_kbn,
+        '_dc': unix_time
+    })
+    url = f'{BASE_URL}/net/schedule/TNPI3050J02.do?{query}'
 
     res = requests.get(url)
     res.encoding = res.apparent_encoding
